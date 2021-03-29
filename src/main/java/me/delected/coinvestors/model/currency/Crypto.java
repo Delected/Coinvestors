ac4940d46d842de22a6b28b5db823fd76f7b671c
rigob <ge35sot@mytum.de>
2021-03-29 14:55:04 +0200

    some qol changes

--- src/main/java/me/delected/coinvestors/model/currency/Crypto.java
+++ src/main/java/me/delected/coinvestors/model/currency/Crypto.java
@@ -1,19 +1,19 @@
 package me.delected.coinvestors.model.currency;
 
-import com.google.common.collect.Sets;
+import static me.delected.coinvestors.util.Sets.newSet;
 
 import java.util.Set;
 
 // not all of these are 100% correct, feel free to change if you see errors in prefixes, lengths, etc
 public enum Crypto {
-    BTC("Bitcoin", Sets.newHashSet("1", "3", "bc1"), 26, 35),
-    ETH("Ethereum", Sets.newHashSet("0x"), 42),
-    BNB("Binance Coin", Sets.newHashSet("bnb1"), 42),
-    ADA("Cardano", Sets.newHashSet("addr1", "DDzFFz", "Ae2td"), 104),
-    DOT("Polkadot", Sets.newHashSet("1"), 47, 48),
-    XRP("Ripple", Sets.newHashSet("r"), 25, 35),
-    LTC("Litecoin", Sets.newHashSet("L", "3", "M"), 26, 33),
-    BCH("Bitcoin Cash", Sets.newHashSet("1", "3", "q", "Q", "p", "P", "bitcoincash:", "BITCOINCASH:"), 34, 41);
+	BTC("Bitcoin", newSet("1", "3", "bc1"), 26, 35),
+	ETH("Ethereum", newSet("0x"), 42),
+	BNB("Binance Coin", newSet("bnb1"), 42),
+	ADA("Cardano", newSet("addr1", "DDzFFz", "Ae2td"), 104),
+	DOT("Polkadot", newSet("1"), 47, 48),
+	XRP("Ripple", newSet("r"), 25, 35),
+	LTC("Litecoin", newSet("L", "3", "M"), 26, 33),
+	BCH("Bitcoin Cash", newSet("1", "3", "q", "Q", "p", "P", "bitcoincash:", "BITCOINCASH:"), 34, 41);
 //    XLM("Stellar"),
 //    WBTC("Wrapped Bitcoin"),
 //    DOGE("Dogecoin"),
@@ -36,27 +36,39 @@ public enum Crypto {
 //    ZEC("Zcash");
 
 
-    private String fullName;
-    private Set<String> prefixes;
-    private int minLength;
-    private int maxLength;
+	private String fullName;
+	private Set<String> prefixes;
+	private int minLength;
+	private int maxLength;
 
-    Crypto(String fullName, Set<String> prefixes, int minLength, int maxLength) {
-        this.fullName = fullName;
-        this.prefixes = prefixes;
-        this.minLength = minLength;
-        this.maxLength = maxLength;
-    }
+	Crypto(String fullName, Set<String> prefixes, int minLength, int maxLength) {
+		this.fullName = fullName;
+		this.prefixes = prefixes;
+		this.minLength = minLength;
+		this.maxLength = maxLength;
+	}
 
-    // for keys with the same min and max length
-    Crypto(String fullName, Set<String> prefixes, int minLength) {
-        this.fullName = fullName;
-        this.prefixes = prefixes;
-        this.minLength = minLength;
-    }
+	// for keys with the same min and max length
+	Crypto(String fullName, Set<String> prefixes, int length) {
+		this.fullName = fullName;
+		this.prefixes = prefixes;
+		this.minLength = length;
+	}
 
-    public String getFullName() { return fullName; }
-    public Set<String> getPrefixes() { return prefixes; }
-    public int getMinLength() { return minLength; }
-    public int getMaxLength() { return maxLength; }
+	public String getFullName() {
+		return fullName;
+	}
+
+	public Set<String> getPrefixes() {
+		return prefixes;
+	}
+
+	public int getMinLength() {
+		return minLength;
+	}
+
+	public int getMaxLength() {
+		return maxLength;
+	}
+
 }
