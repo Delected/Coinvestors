83680ac2b7674666607d739d9e77788a22e25a79
rigob <ge35sot@mytum.de>
2021-03-30 12:39:05 +0200

    little fix for making the project compile, will be reverted when new generation structure is out

--- src/main/java/me/delected/coinvestors/model/currency/Crypto.java
+++ src/main/java/me/delected/coinvestors/model/currency/Crypto.java
@@ -15,35 +15,34 @@ import java.util.Set;
 */
 
 public enum Crypto {
-    BTC("Bitcoin", Sets.newSet("1", "3", "bc1"), 26, 35),
-    ETH("Ethereum", Sets.newSet("0x"), 42),
-    BNB("Binance Coin", Sets.newSet("bnb1"), 42),
-    ADA("Cardano", Sets.newSet("addr1", "DDzFFz", "Ae2td"), 104),
-    DOT("Polkadot", Sets.newSet("1"), 47, 48),
-    XRP("Ripple", Sets.newSet("r"), 25, 35),
-    LTC("Litecoin", Sets.newSet("L", "3", "M"), 26, 33),
-    BCH("Bitcoin Cash", Sets.newSet("1", "3", "q", "Q", "p", "P", "bitcoincash:", "BITCOINCASH:"), 34, 41),
-    XLM("Stellar", Sets.newSet("G"), 56), // address is in all caps
-    WBTC("Wrapped Bitcoin", Sets.newSet("0x"), 42), // same as ETH, WBTC is basically BTC used with ETH tokens
-    DOGE("Dogecoin", Sets.newSet("D"), 33, 35), // D, followed by any capital letter or number. the rest is random
-    HEX("HEX", Sets.newSet("0x"), 42), // same as ETH, HEX uses ETH tokens
-    XMR("Monero", Sets.newSet("4"), 93), // 4[0-9AB][123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{93}
-    EOS("EOS", 12), // IMPORTANT: (account name, probably use player username) 12 long - a-z (lowercase), 1-5 and . (period)
-    USDT("Tether", Sets.newSet("0x"), 42), // same as ETH
-    UNI("Uniswap", Sets.newSet("0x"), 42), // same as ETH
-    THETHA("THETA", Sets.newSet("0x"), 42), // same as ETH 
-    LINK("ChainLink"),
-    USDC("USD Coin"),
-    ZRX("0x"),
-    BUSD("Binance USD"),
-    TRX("TRON"),
-    FIL("Filecoin"),
-    BSV("Bitcoin SV"),
-    VET("VeChain"),
-    ETC("Ethereum Classic"),
-    DASH("Dash"),
-    ZEC("Zcash");
-
+	BTC("Bitcoin", Sets.newSet("1", "3", "bc1"), 26, 35),
+	ETH("Ethereum", Sets.newSet("0x"), 42),
+	BNB("Binance Coin", Sets.newSet("bnb1"), 42),
+	ADA("Cardano", Sets.newSet("addr1", "DDzFFz", "Ae2td"), 104),
+	DOT("Polkadot", Sets.newSet("1"), 47, 48),
+	XRP("Ripple", Sets.newSet("r"), 25, 35),
+	LTC("Litecoin", Sets.newSet("L", "3", "M"), 26, 33),
+	BCH("Bitcoin Cash", Sets.newSet("1", "3", "q", "Q", "p", "P", "bitcoincash:", "BITCOINCASH:"), 34, 41),
+	XLM("Stellar", Sets.newSet("G"), 56), // address is in all caps
+	WBTC("Wrapped Bitcoin", Sets.newSet("0x"), 42), // same as ETH, WBTC is basically BTC used with ETH tokens
+	DOGE("Dogecoin", Sets.newSet("D"), 33, 35), // D, followed by any capital letter or number. the rest is random
+	HEX("HEX", Sets.newSet("0x"), 42), // same as ETH, HEX uses ETH tokens
+	XMR("Monero", Sets.newSet("4"), 93), // 4[0-9AB][123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{93}
+	EOS("EOS", 12), // IMPORTANT: (account name, probably use player username) 12 long - a-z (lowercase), 1-5 and . (period)
+	USDT("Tether", Sets.newSet("0x"), 42), // same as ETH
+	UNI("Uniswap", Sets.newSet("0x"), 42), // same as ETH
+	THETHA("THETA", Sets.newSet("0x"), 42), // same as ETH
+	LINK("ChainLink"),
+	USDC("USD Coin"),
+	ZRX("0x"),
+	BUSD("Binance USD"),
+	TRX("TRON"),
+	FIL("Filecoin"),
+	BSV("Bitcoin SV"),
+	VET("VeChain"),
+	ETC("Ethereum Classic"),
+	DASH("Dash"),
+	ZEC("Zcash");
 
 
 	private String fullName;
@@ -56,6 +55,11 @@ public enum Crypto {
 	Crypto(String fullName, AddressGenerator generator) {
 		this.fullName = fullName;
 		this.generator = generator;
+	}
+
+	@Deprecated
+	Crypto(String todo) {
+
 	}
 
 	@Deprecated
@@ -73,13 +77,13 @@ public enum Crypto {
 		this.prefixes = prefixes;
 		this.minLength = length;
 	}
-	
+
 	// for keys that need a different generation method
 	@Deprecated
 	Crypto(String fullName, int length) {
 		this.fullName = fullName;
-		this.length = length;
-	} 
+		this.minLength = this.maxLength = length;
+	}
 
 	public String getFullName() {
 		return fullName;
@@ -89,10 +93,12 @@ public enum Crypto {
 		return prefixes;
 	}
 
+	@Deprecated
 	public int getMinLength() {
 		return minLength;
 	}
 
+	@Deprecated
 	public int getMaxLength() {
 		return maxLength;
 	}
