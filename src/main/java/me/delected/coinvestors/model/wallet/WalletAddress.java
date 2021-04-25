79746cca90b37462b1bc739cc2c35add809d7743
BuildTools <fkafieh@gmail.com>
2021-04-25 15:57:23 -0400

    made WalletAddress an interface

--- src/main/java/me/delected/coinvestors/model/wallet/WalletAddress.java
+++ src/main/java/me/delected/coinvestors/model/wallet/WalletAddress.java
@@ -2,12 +2,7 @@ package me.delected.coinvestors.model.wallet;
 
 import java.math.BigInteger;
 
-public class WalletAddress {
-    public BigInteger publicKey() {
-
-    }
-
-    public BigInteger privateKey() {
-
-    }
+public interface WalletAddress {
+    BigInteger publicKey();
+    BigInteger privateKey();
 }
---
+++ src/main/java/me/delected/coinvestors/model/wallet/WalletAddressImpl.java
@@ -0,0 +1,16 @@
+package me.delected.coinvestors.model.wallet;
+
+import java.math.BigInteger;
+
+public class WalletAddressImpl implements WalletAddress {
+
+    @Override
+    public BigInteger publicKey() {
+        return null; //TODO
+    }
+
+    @Override
+    public BigInteger privateKey() {
+        return null; //TODO
+    }
+}
