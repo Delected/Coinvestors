84ab255400ccb62469431f840802f3d98caedd30
rigob <ge35sot@mytum.de>
2021-04-29 20:39:26 +0200

    registering Listeners automatically

--- src/main/java/me/delected/coinvestors/Coinvestors.java
+++ src/main/java/me/delected/coinvestors/Coinvestors.java
@@ -8,12 +8,14 @@ import me.delected.coinvestors.controller.CryptoRegulator;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.PluginCommand;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.delected.coinvestors.commands.CommandDistributor;
 import me.delected.coinvestors.controller.GuiPlayerStateManager;
 import me.delected.coinvestors.exceptions.ContactTheDevsException;
 import me.delected.coinvestors.io.yaml.Yaml;
+import me.delected.coinvestors.listeners.AbstractListener;
 
 public final class Coinvestors extends JavaPlugin {
 	private static Coinvestors INSTANCE;
@@ -28,16 +30,20 @@ public final class Coinvestors extends JavaPlugin {
 	public void onEnable() {
 		// have a log of all transactions maybe
 		saveDefaultConfig();
+		//register commands
 		Optional<PluginCommand> cvCommand = Optional.ofNullable(getCommand("coinvestors"));
 		CommandDistributor distributor;
 		cvCommand.orElseThrow(ContactTheDevsException::new).setExecutor((distributor = new CommandDistributor()));
 		cvCommand.get().setTabCompleter(distributor);
+		//register all Listeners
+		PluginManager pluginManager = getServer().getPluginManager();
+		AbstractListener.createListeners().forEach(l -> pluginManager.registerEvents(l, this));
+		//DEBUG
 		Objects.requireNonNull(getCommand("open")).setExecutor(new OpenCommand());
 		/*
 		Bukkit.getScheduler().runTaskTimer(this, CryptoRegulator::recalculatePrices,
 				0L, getConfig().getInt("reload_seconds") * 1000L);*/
 	}
-
 
 	public static JavaPlugin INSTANCE() {
 		return INSTANCE;
--- src/main/java/me/delected/coinvestors/listeners/AbstractListener.java
+++ src/main/java/me/delected/coinvestors/listeners/AbstractListener.java
@@ -1,10 +1,44 @@
 package me.delected.coinvestors.listeners;
 
+import java.lang.reflect.Modifier;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.bukkit.Bukkit;
 import org.bukkit.event.Event;
 import org.bukkit.event.Listener;
+import org.reflections.Reflections;
+
+import me.delected.coinvestors.commands.AbstractCommand;
 
 public abstract class AbstractListener<E extends Event> implements Listener {
 
 	public abstract void handle(E event);
+
+	public static Set<Listener> createListeners() {
+		Reflections reflections = new Reflections("me.delected.coinvestors.commands");
+		return reflections.getSubTypesOf(AbstractListener.class).stream()
+				//remove abstract classes and interfaces in that packages
+				.filter(command -> !Modifier.isAbstract(command.getModifiers()) && !Modifier.isInterface(command.getModifiers()))
+				//make sure the default constructor exists
+				.filter(command -> Stream.of(command.getDeclaredConstructors()).anyMatch(constructor -> constructor.getParameterCount() == 0))
+				//create a new instance using this constructor
+				.map(commandClass -> {
+					try {
+						return commandClass.getDeclaredConstructor().newInstance();
+					} catch (ReflectiveOperationException e) {
+						Bukkit.getLogger().severe("Could not initialize the " + commandClass.getSimpleName());
+						Bukkit.getLogger().severe(e.getMessage() + e.getCause());
+						return null;
+					}
+				})
+				//everything should have worked fine.
+				//If not, there is a bug that has to be fixed!
+				//But to continue here we just remove this null-Object.
+				.filter(Objects::nonNull)
+				.collect(Collectors.toSet());
+	}
 
 }
