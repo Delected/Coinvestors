86628ec12c5d119d7db8b15c75584b28c55ba2f3
BuildTools <fkafieh@gmail.com>
2021-05-22 17:06:56 -0400

    csv model additions

---
+++ src/main/java/me/delected/coinvestors/listeners/world/WorldSaveListener.java
@@ -0,0 +1,11 @@
+package me.delected.coinvestors.listeners.world;
+
+import me.delected.coinvestors.listeners.AbstractListener;
+import org.bukkit.event.world.WorldSaveEvent;
+
+public class WorldSaveListener extends AbstractListener<WorldSaveEvent> {
+    @Override
+    public void handle(WorldSaveEvent event) {
+        // TODO - save csv here
+    }
+}
