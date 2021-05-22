56c2908855ed9e39bf592c6114b5544242298a6e
BuildTools <fkafieh@gmail.com>
2021-05-22 17:10:38 -0400

    forgot how csv works for a sec not a big deal

--- src/main/java/me/delected/coinvestors/io/csv/CsvWriter.java
+++ src/main/java/me/delected/coinvestors/io/csv/CsvWriter.java
@@ -5,6 +5,7 @@ import me.delected.coinvestors.exceptions.IncorrectFileTypeException;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,9 +21,8 @@ public class CsvWriter extends CsvAccessor {
 
 	public void addRow(String... strings) {
 		try (BufferedWriter write = new BufferedWriter(new FileWriter(file))) {
-			for (String s : strings) {
-				write.append(s);
-			}
+			String s = String.join(delimiter, strings);
+			write.append(s);
 			write.flush();
 		} catch (IOException e) {
 			e.printStackTrace();
