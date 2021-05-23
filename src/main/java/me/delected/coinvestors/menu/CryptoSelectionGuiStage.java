a20c2d057cfeb19e35cf7d2db1464d60800235d7
rigob <ge35sot@mytum.de>
2021-05-23 12:49:43 +0200

    extracted the pageGui

--- src/main/java/me/delected/coinvestors/menu/CryptoSelectionGuiStage.java
+++ src/main/java/me/delected/coinvestors/menu/CryptoSelectionGuiStage.java
@@ -5,12 +5,10 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import me.delected.coinvestors.Coinvestors;
@@ -18,35 +16,22 @@ import me.delected.coinvestors.controller.MenuLinker;
 import me.delected.coinvestors.model.currency.Crypto;
 import me.delected.coinvestors.util.ItemStackCreator;
 
-public class CryptoSelectionGuiStage extends GuiStage {
+public class CryptoSelectionGuiStage extends PagedGui {
 
-	private static final String NEXT_PAGE = "CRYPTO_SELECT_NEXT_PAGE";
-	private static final String PREV_PAGE = "CRYPTO_SELECT_PREV_PAGE";
 	private static final String SELECT_CONFIRM = "CRYPTO_SELECTION_CONFIRM";
-	private static final List<ItemStack> stacks = createInfoStacks();
+	private static final List<ItemStack> STACKS = createInfoStacks();
 	private static final String TITLE = ChatColor.DARK_RED + "Select your crypto currency!";
 
 	static {
-		MenuLinker.registerLink(NEXT_PAGE, CryptoSelectionGuiStage::displayNextPage);
-		MenuLinker.registerLink(PREV_PAGE, CryptoSelectionGuiStage::displayPrevPage);
 		MenuLinker.registerEventLink(SELECT_CONFIRM, CryptoSelectionGuiStage::selectCrypto);
 	}
 
-	private final Inventory inventory;
-	private int page = 0;
 	private final Consumer<Crypto> onSelect;
 
 	protected CryptoSelectionGuiStage(final Consumer<Crypto> onSelect) {
-		super(MenuState.CRYPTO_SELECT);
+		super(MenuState.CRYPTO_SELECT, 54, TITLE);
 		this.onSelect = onSelect;
-		inventory = Bukkit.createInventory(null, 54, TITLE);
 		buildPageInventory();
-	}
-
-
-	@Override
-	public Inventory build(final Player player) {
-		return inventory;
 	}
 
 	private static List<ItemStack> createInfoStacks() {
@@ -58,47 +43,14 @@ public class CryptoSelectionGuiStage extends GuiStage {
 				.setEventLink(SELECT_CONFIRM).build();
 	}
 
-	private ItemStack nextPage() {
-		return new ItemStackCreator(Material.LIME_STAINED_GLASS_PANE).setName("Next page").setLink(NEXT_PAGE).build();
-	}
-
-	private ItemStack prevPage() {
-		return new ItemStackCreator(Material.LIME_STAINED_GLASS_PANE).setName("Previous page")
-				.setLink(PREV_PAGE).build();
-	}
-
-	private void buildPageInventory() {
-		inventory.clear();
-		inventory.setItem(45, prevPage());
-		inventory.setItem(53, nextPage());
-		stacks.stream().skip(page * 45L).limit(45L).forEach(inventory::addItem);
-	}
-
-	private static void displayNextPage(Player player) {
-		((CryptoSelectionGuiStage) Coinvestors.guiManager().getStateOf(player).getActualStage()).displayNextPage();
-	}
-
-	private static void displayPrevPage(Player player) {
-		((CryptoSelectionGuiStage) Coinvestors.guiManager().getStateOf(player).getActualStage()).displayPrevPage();
-	}
-
-	private void displayNextPage() {
-		if (page < stacks.size() / 45) {
-			page++;
-			buildPageInventory();
-		}
-	}
-
-	private void displayPrevPage() {
-		if (page > 0) {
-			page--;
-			buildPageInventory();
-		}
+	@Override
+	protected List<ItemStack> getRawItems() {
+		return STACKS;
 	}
 
 	private static void selectCrypto(Player player, InventoryClickEvent e) {
 		CryptoSelectionGuiStage guiStage = (CryptoSelectionGuiStage) Coinvestors.guiManager().getStateOf(player).getActualStage();
-		Crypto crypto = Crypto.values()[45 * guiStage.page + e.getSlot()];
+		Crypto crypto = Crypto.values()[guiStage.getFirstField() + e.getSlot()];
 		guiStage.onSelect.accept(crypto);
 	}
 
--- src/main/java/me/delected/coinvestors/menu/PagedGui.java
+++ src/main/java/me/delected/coinvestors/menu/PagedGui.java
@@ -2,28 +2,57 @@ package me.delected.coinvestors.menu;
 
 import java.util.List;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 import me.delected.coinvestors.Coinvestors;
+import me.delected.coinvestors.controller.MenuLinker;
+import me.delected.coinvestors.util.ItemStackCreator;
 
-public abstract class PagedGui<T> extends GuiStage {
-
-	private final int size = 45;
-	private List<T> rawItems;
-	private int page = 0;
+public abstract class PagedGui extends GuiStage {
 
 	private static final String NEXT_PAGE = "PAGED_GUI_NEXT_PAGE";
 	private static final String PREV_PAGE = "PAGED_GUI_PREV_PAGE";
-	private static final String ITEM_SELECT = "PAGED_GUI_SELECTION_CONFIRM";
 
-	protected PagedGui(final MenuState state) {
-		super(state);
+	static {
+		MenuLinker.registerLink(NEXT_PAGE, PagedGui::displayNextPage);
+		MenuLinker.registerLink(PREV_PAGE, PagedGui::displayPrevPage);
 	}
 
-	protected abstract void buildPageInventory();
+	private final int size;
+	private final int itemSpace;
+	private int page = 0;
+	private final Inventory inventory;
+
+	protected PagedGui(final MenuState state, final int size, String title) {
+		super(state);
+		if (size < 18) {
+			throw new IllegalArgumentException("Inventory must have at least 2 full rows!");
+		}
+		this.size = size;
+		inventory = Bukkit.createInventory(null, size, title);
+		this.itemSpace = size - 9;
+	}
+
+	@Override
+	public Inventory build(final Player player) {
+		return inventory;
+	}
+
+	protected void buildPageInventory() {
+		inventory.clear();
+		inventory.setItem(itemSpace, prevPage());
+		inventory.setItem(size - 1, nextPage());
+		getRawItems().stream().skip((long) page * itemSpace).limit(itemSpace).forEach(inventory::addItem);
+	}
+
+	protected abstract List<ItemStack> getRawItems();
 
 	private void displayNextPage() {
-		if (page < rawItems.size() / 45) {
+		if (page < getRawItems().size() / itemSpace) {
 			page++;
 			buildPageInventory();
 		}
@@ -36,12 +65,24 @@ public abstract class PagedGui<T> extends GuiStage {
 		}
 	}
 
+	private ItemStack nextPage() {
+		return new ItemStackCreator(Material.LIME_STAINED_GLASS_PANE).setName("Next page").setLink(NEXT_PAGE).build();
+	}
+
+	private ItemStack prevPage() {
+		return new ItemStackCreator(Material.LIME_STAINED_GLASS_PANE).setName("Previous page")
+				.setLink(PREV_PAGE).build();
+	}
+
 	private static void displayNextPage(Player player) {
-		((PagedGui<?>) Coinvestors.guiManager().getStateOf(player).getActualStage()).displayNextPage();
+		((PagedGui) Coinvestors.guiManager().getStateOf(player).getActualStage()).displayNextPage();
 	}
 
 	private static void displayPrevPage(Player player) {
-		((PagedGui<?>) Coinvestors.guiManager().getStateOf(player).getActualStage()).displayPrevPage();
+		((PagedGui) Coinvestors.guiManager().getStateOf(player).getActualStage()).displayPrevPage();
 	}
 
+	protected int getFirstField() {
+		return page * itemSpace;
+	}
 }
