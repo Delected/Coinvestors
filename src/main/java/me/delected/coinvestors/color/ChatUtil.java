cf4d69af617c7a5d591ab4b40aa549e3c0dcd1ea
rigob <ge35sot@mytum.de>
2021-05-23 12:29:03 +0200

    made a Builder to translate color codes easier

--- src/main/java/me/delected/coinvestors/color/ChatUtil.java
+++ src/main/java/me/delected/coinvestors/color/ChatUtil.java
@@ -7,25 +7,56 @@ import java.util.regex.Pattern;
 
 /**
  * used for converting strings and hex color codes into colored strings
- *
+ * <p>
  * credits to https://www.spigotmc.org/threads/1-16-1-new-color-utils-rgb-and-hex-parsing.451068/
  */
 public class ChatUtil {
-    private static final Pattern HEX_PATTERN = Pattern.compile("<#([A-Fa-f0-9]){6}>");
+	private static final Pattern HEX_PATTERN = Pattern.compile("<#([A-Fa-f0-9]){6}>");
 
-    public static String color(String s) {
-        if (s == null || s.equals("")) {
-            return "";
-        }
-        Matcher matcher = HEX_PATTERN.matcher(s);
 
-        while (matcher.find()) {
-            final ChatColor hexColor = ChatColor.of(matcher.group().substring(1, matcher.group().length() - 1));
-            final String before = s.substring(0, matcher.start());
-            final String after = s.substring(matcher.end());
-            s = before + hexColor + after;
-            matcher = HEX_PATTERN.matcher(s);
-        }
-        return ChatColor.translateAlternateColorCodes('&', s);
-    }
+	public static String color(String s) {
+		if (s == null || s.equals("")) {
+			return "";
+		}
+		Builder builder = new Builder();
+		Matcher matcher = HEX_PATTERN.matcher(s);
+
+		while (matcher.find()) {
+			builder.text(s.substring(0, matcher.start()));
+			builder.hex(matcher.group().substring(1, matcher.group().length() - 1));
+			matcher = HEX_PATTERN.matcher(s.substring(matcher.end()));
+		}
+		return builder.build();
+	}
+
+	public static class Builder {
+
+		private static final char ALT_COLOR_CHAR = '&';
+
+		private final StringBuilder stringBuilder = new StringBuilder();
+
+		public Builder hex(String hex) {
+			stringBuilder.append(fromHex(hex));
+			return this;
+		}
+
+		private ChatColor fromHex(String hex) {
+			return ChatColor.of(hex);
+		}
+
+		public Builder cColor(ChatColor color) {
+			stringBuilder.append(color);
+			return this;
+		}
+
+		public Builder text(String text) {
+			stringBuilder.append(text);
+			return this;
+		}
+
+		public String build() {
+			return ChatColor.translateAlternateColorCodes(ALT_COLOR_CHAR, stringBuilder.toString());
+		}
+	}
+
 }
---
+++ src/main/java/me/delected/coinvestors/menu/WalletSelectionGUIStage.java
@@ -0,0 +1,17 @@
+package me.delected.coinvestors.menu;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+public class WalletSelectionGUIStage extends GuiStage {
+
+
+	protected WalletSelectionGUIStage(final MenuState state) {
+		super(state);
+	}
+
+	@Override
+	public Inventory build(final Player player) {
+		return null;
+	}
+}
