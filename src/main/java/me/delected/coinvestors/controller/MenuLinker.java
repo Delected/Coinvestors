bfdcee26ad7a706ccb9ad7155e9c9adc6238933f
rigob <ge35sot@mytum.de>
2021-05-24 16:00:32 +0200

    exception handling and debug info

--- src/main/java/me/delected/coinvestors/controller/MenuLinker.java
+++ src/main/java/me/delected/coinvestors/controller/MenuLinker.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
@@ -13,11 +14,11 @@ public class MenuLinker {
 	private static final Map<String, BiConsumer<Player, InventoryClickEvent>> EVENT_LINK_ACTIONS = new HashMap<>();
 
 	public static Consumer<Player> getAction(String link) {
-		return LINK_ACTIONS.get(link);
+		return LINK_ACTIONS.getOrDefault(link, p -> warn(p, link));
 	}
 
 	public static BiConsumer<Player, InventoryClickEvent> getEventAction(String link) {
-		return EVENT_LINK_ACTIONS.get(link);
+		return EVENT_LINK_ACTIONS.getOrDefault(link, (p, e) -> warn(p, link));
 	}
 
 	public static void registerLink(String link, Consumer<Player> action) {
@@ -26,5 +27,9 @@ public class MenuLinker {
 
 	public static void registerEventLink(String link, BiConsumer<Player, InventoryClickEvent> eventBiConsumer) {
 		EVENT_LINK_ACTIONS.put(link, eventBiConsumer);
+	}
+
+	private static void warn(Player player, String link) {
+		Bukkit.getLogger().warning(player + " tried to use link " + link + " which had no mapping!");
 	}
 }
