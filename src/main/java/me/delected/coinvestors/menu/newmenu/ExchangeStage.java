30f79f9261df65d93a5710ff5f8355451b7bb458
rigob <ge35sot@mytum.de>
2021-06-03 19:27:41 +0200

    began the new ExchangeStage - follow the TODOs to finish

---
+++ src/main/java/me/delected/coinvestors/menu/newmenu/ExchangeStage.java
@@ -0,0 +1,116 @@
+package me.delected.coinvestors.menu.newmenu;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import me.delected.coinvestors.controller.MenuLinker;
+import me.delected.coinvestors.menu.GuiStage;
+import me.delected.coinvestors.model.accounts.Wallet;
+
+/* Todo summary: create links and static methods like the already
+    existing one, put the right ones in the ItemStack creation methods, register the links,
+       call the exchange method of the AccountService in the confirmAction method,
+       Finally, remove the todos ;)
+ */
+public class ExchangeStage extends ReturningGuiStage implements Confirmable {
+	//CONSTANTS
+	private static final String TITLE = ChatColor.YELLOW + "Exchange crypto";
+	private static final int SIZE = 36;
+	//LINKS TODO: create the other ones
+	private static final String SOURCE_CRYPTO_INPUT_LINK = "EXCHANGE_SOURCE_C_INPUT";
+
+	//LINK REGISTRATION TODO: register the recently created ones
+	static {
+		MenuLinker.registerLink(SOURCE_CRYPTO_INPUT_LINK, ExchangeStage::openSourceCryptoInput);
+	}
+
+	//INPUT PROVIDERS
+	private final CryptoInputGui sourceCryptoInputGui = new CryptoInputGui(this);
+	private final CryptoInputGui targetCryptoInputGui = new CryptoInputGui(this);
+	private final AmountInputGui amountInputGui = new AmountInputGui(this);
+	private final RestrictedInputGui<Wallet> sourceWalletInputGui;
+	private final RestrictedInputGui<Wallet> targetWalletInputGui;
+
+	//Constructor
+	protected ExchangeStage(GuiStage previous) {
+		super(MenuState.EXCHANGE, previous);
+		//Init of the restricted guis with underlying WalletInputGuis and dependency on the regarding crypto input
+		WalletInputGui sourceWalletInputGui = new WalletByCryptoInputGui(this, sourceCryptoInputGui::getData);
+		this.sourceWalletInputGui = new RestrictedInputGui<>(this, sourceWalletInputGui, sourceCryptoInputGui);
+		WalletInputGui targetWalletInputGui = new WalletByCryptoInputGui(this, sourceCryptoInputGui::getData);
+		this.targetWalletInputGui = new RestrictedInputGui<>(this, targetWalletInputGui, sourceCryptoInputGui);
+
+	}
+
+	//TODO: put the created link strings on their place
+	@Override
+	public Inventory build(final Player player) {
+		Inventory result = Bukkit.createInventory(null, SIZE, TITLE);
+		result.setItem(11, sourceCryptoInputGui.getInfoStack(SOURCE_CRYPTO_INPUT_LINK));
+		result.setItem(15, targetCryptoInputGui.getInfoStack("null"));
+		result.setItem(20, sourceWalletInputGui.getInfoStack("null"));
+		result.setItem(22, amountInputGui.getInfoStack("null"));
+		result.setItem(24, targetWalletInputGui.getInfoStack("null"));
+		result.setItem(34, confirmStack(CONFIRM_LINK));
+		ItemStack rs = returnStack(ChatColor.RED + "Return to previous menu",
+				ChatColor.WHITE + "Aborts the exchange");
+		result.setItem(35, rs);
+		return result;
+	}
+
+	@Override
+	public List<String> invalids() {
+		List<String> invalids = new ArrayList<>();
+		if (sourceCryptoInputGui.getData() == null) {
+			invalids.add("Source crypto");
+		}
+		if (sourceWalletInputGui.getData() == null) {
+			invalids.add("Source wallet");
+		}
+		if (amountInputGui.getData() == null) {
+			invalids.add("transaction amount");
+		}
+		if (targetCryptoInputGui.getData() == null) {
+			invalids.add("Destination crypto");
+		}
+		if (targetWalletInputGui.getData() == null) {
+			invalids.add("Destination wallet");
+		}
+		return invalids;
+	}
+
+	//helper method which gets the instance of this class the player is using at the moment
+	private static ExchangeStage getGui(Player player) {
+		return actualStage(player, ExchangeStage.class);
+	}
+
+	//working link action
+	private static void openSourceCryptoInput(Player player) {
+		getGui(player).sourceCryptoInputGui.open(player);
+	}
+	//TODO: Create link actions for the created links
+
+
+	//TODO: Perform the exchange using the AccountService
+	@Override
+	public Consumer<Player> confirmAction() {
+		return p -> {
+		};
+	}
+
+	//Checks if all input is done
+	@Override
+	public boolean isValid() {
+		return Stream.of(sourceWalletInputGui, targetWalletInputGui, amountInputGui,
+				sourceCryptoInputGui, targetCryptoInputGui).noneMatch(i -> Objects.isNull(i.getData()));
+	}
+}
